8380b7e104cd04d79c596f26926f0da1d43d4bf0
Diego Salvador <104446784+DiegoSalvador27@users.noreply.github.com>
2022-12-07 16:34:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,61 +7,54 @@ from datetime import datetime
 alreadyRan = False
 video_capture = cv2.VideoCapture(0)
 blocking = False
-# Load a sample picture and learn how to recognize it.
-Diego_image = face_recognition.load_image_file("Diego.jpg")
-Diego_face_encoding = face_recognition.face_encodings(Diego_image)[0]
-
-# Load a second sample picture and learn how to recognise it.
-Charles_image = face_recognition.load_image_file("Charles.jpg")
-Charles_face_encoding = face_recognition.face_encodings(Charles_image)[0]
+# This will take a jpg and learn the facial features and store it.
+<your_name>_image = face_recognition.load_image_file("<your_image>.jpg")
+<your_name>_encoding = face_recognition.face_encodings(<your_name>_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    Diego_face_encoding,
-    Charles_face_encoding
+    <your_name>_face_encoding,
 ]
 known_face_names = [
-    "Diego(OWNER)",
-    "Charles(the_guy_that_MrHoffman_calles_whenever_he's_bored"
+    "<your_name>(OWNER)",
 ]
 
-# Initialize some variables
+# just some variables
 face_locations = []
 face_encodings = []
 face_names = []
 process_this_frame = True
 while True:
-    # Grab a single frame of video
+    # grab a frame of camera/webcam
     ret, frame = video_capture.read()
 
-    # Only process every other frame of video to save time
+    # proscesses every other frame coz facereq is hard to proscess:(
     if process_this_frame:
-        # Resize frame of video to 1/4 size for faster face recognition processing
+        # make the frame smaller coz once again facereq eats more ram than chrome
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        # turn the brg frame to rgb because cv2 see's things like a tv 1937(black and white) 
         rgb_small_frame = small_frame[:, :, ::-1]
 
-        # Find all the faces and face encodings in the current frame of video
+        # check if there are any faces and load the location and encoding
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
+            # check if the face on frame matches any jpg listed above
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding, 0.65)
             name = "Unknown"
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
-
+            
             # Or instead, use the known face with the smallest distance to the new face
+            # i started to fall asleep so i have no idea what i was doing
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
             face_names.append(name)
+            
+            # if the person still cant be matched to a photo start playing an alarm and write Unknown Person recorded at the time it happend 
             if name == "Unknown":
                 playsound.playsound("alarm.wav", block=blocking)
             with open("unknownEntries.txt", mode='a') as file:
@@ -69,27 +62,27 @@ while True:
 
     process_this_frame = not process_this_frame
 
-    # Display the results
+    # show the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        # we get the video back to its original size before showing it
         top *= 4
         right *= 4
         bottom *= 4
         left *= 4
 
-        # Draw a box around the face
+        # create a visible box around the face to indicate a face was found
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a label with a name below the face
+        # display the name of the recognised person or display unknown
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-    # Display the resulting image
+    # finaly create a frame and desplay it on the screen.
     cv2.imshow('Video', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release handle to the webcam
+# kill the process
 video_capture.release()
 cv2.destroyAllWindows()
